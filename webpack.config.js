ede4df6b908dacf0653833daf59072f24a495f80
Eduardo Magaldi <magaldi1989@gmail.com>
2017-12-10 17:27:11 -0200

    trying hackerhank honeypot challenge

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+import webpack from 'webpack';
+import path from 'path';
 
-module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+export default {
+	debug: true,
+	devtool: 'cheap-module-eval-source-map',
+	noInfo: false,
+	entry: [
+		'eventsource-polyfill',
+		'webpack-hot-middleware/client?reload=true',
+		'./src/index'
+	],
+	target: 'web',
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './src'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [
+			{test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+			{test: /(\.css)$/, loaders: ['style', 'css']},
+			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+			{test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+			{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+			{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+		]
+	}
 };
